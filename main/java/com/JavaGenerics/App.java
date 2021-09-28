4dccc80e36e80ec0e3c61ebdbf2e2aedfe01a822
sonam1812 <sonampal832@gmail.com>
2021-09-28 21:16:39 +0530

    Added: Generics Example

---
+++ main/java/com/JavaGenerics/App.java
@@ -0,0 +1,13 @@
+package com.JavaGenerics;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
